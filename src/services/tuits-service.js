f0fe052e4825b3e205235b50c1cb6f8f0b711aab
johnnn-117 <hao.junh@northeastern.edu>
2022-11-18 11:36:04 -0800

    update

--- src/services/tuits-service.js
+++ src/services/tuits-service.js
@@ -13,7 +13,7 @@ export const findTuits  = async ()     => {
     return tuits;
 
 }
-export const deleteTuit = async (tuit) => {
+export const deleteTuit = async (tid) => {
     const response = await axios
         .delete(`${TUITS_API}/${tid}`)
     return response.data
